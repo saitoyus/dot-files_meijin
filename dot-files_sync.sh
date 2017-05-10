cbdf2d4089c4c29cfd62ba01ddbda1535bb5b80c
SaitoYusuke <saitoyus@unit-nus.homes.co.jp>
2017-05-10 15:35:01 +0900

    sh file modified about directory moving

--- dot-files_sync.sh
+++ dot-files_sync.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-ln -sf ~/dot-files_meijin/.vim/vimrcAutoload ~/.vim/
-ln -sf ~/dot-files_meijin/.vim/colors ~/.vim/
+ln -snf ~/dot-files_meijin/.vim/vimrcAutoload ~/.vim/
+ln -snf ~/dot-files_meijin/.vim/colors ~/.vim/
 ln -sf ~/dot-files_meijin/.vimrc ~/.vimrc
 ln -sf ~/dot-files_meijin/.tmux.conf ~/.tmux.conf
 ln -sf ~/dot-files_meijin/.bashrc ~/.bashrc
-
+source ~/.bashrc

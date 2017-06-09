1a0d730ca2a259079a8c8a894cdd5e867619912f
SaitoYusuke <saitoyus@unit-nc-001.awsnext>
2017-06-09 11:32:16 +0900

    zshrc追加

--- dot-files_sync.sh
+++ dot-files_sync.sh
@@ -4,6 +4,7 @@ ln -snf ~/dot-files_meijin/.vim/colors ~/.vim/
 ln -sf ~/dot-files_meijin/.vimrc ~/.vimrc
 ln -sf ~/dot-files_meijin/.tmux.conf ~/.tmux.conf
 ln -sf ~/dot-files_meijin/.bashrc ~/.bashrc
+ln -sf ~/dot-files_meijin/.zshrc ~/.zshrc
 
 filename=~/dot-files_meijin/.vimignore
 cat ${filename} | while read line

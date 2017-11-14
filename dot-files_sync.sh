49a4636f39579846d0b05f813fe2a084b4497d55
Yusuke Saito <SaitoYusuke@lifull.com>
2017-11-14 12:47:48 +0900

    orig制度の廃止

--- dot-files_sync.sh
+++ dot-files_sync.sh
@@ -6,14 +6,4 @@ ln -sf ~/dot-files_meijin/.tmux.conf ~/.tmux.conf
 ln -sf ~/dot-files_meijin/.bashrc ~/.bashrc
 ln -sf ~/dot-files_meijin/.zshrc ~/.zshrc
 
-for name in ~/dot-files_meijin/.vim/vimrcAutoload/*.orig; do
-    mv $name ${name%.orig}
-done
-
-filename=~/dot-files_meijin/.vimignore
-cat ${filename} | while read line
-do
-    mv ~/dot-files_meijin/.vim/vimrcAutoload/${line} ~/dot-files_meijin/.vim/vimrcAutoload/${line}.orig
-done
-
 source ~/.zshrc
